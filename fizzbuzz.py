2376ac2fbf617f725dc556474ae33c51f8747c71
devhicks210 <jmatthicks+1@gmail.com>
2016-02-28 21:17:13 -0800

    Added files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+zeefile = open('fizzbuzz.txt')
+
+for x in zeefile:
+	a, b, c = x.strip().split()
+
+	printthis = ''
+
+	for x in range(1, int(c) + 1):
+		if x % int(a) == 0 and x % int(b) == 0:
+			printthis = printthis + "FB "
+		elif x % int(a) == 0:
+			printthis = printthis + "F "
+		elif x % int(b) == 0:
+			printthis = printthis + "B "
+		else:
+			printthis = printthis + str(x) + " "
+
+	print printthis[:-1]
